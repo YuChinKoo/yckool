41b5b719440444f1e77dbc376275d138bf5fc5bd
Indekxx <eugenekoo777@gmail.com>
2020-02-23 15:13:08 -0500

    added another image; background image now switches on refresh

--- src/app/my-website/my-website.component.ts
+++ src/app/my-website/my-website.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-website',
   templateUrl: './my-website.component.html',
   styleUrls: ['./my-website.component.scss']
 })
-export class MyWebsiteComponent implements OnInit {
+export class MyWebsiteComponent implements AfterViewInit {
 
-  constructor() { }
+  public randomNumber: number;
+  public photos: string[] = [
+    'img-sao-aincrad',
+    'img-sao-aincrad-logcabin'
+  ];
+  public image: string;
 
-  ngOnInit() {
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.randomNumber = Math.floor(Math.random() * 2);
+    this.image = this.photos[this.randomNumber];
+    this.cdr.detectChanges();
   }
 
 }
