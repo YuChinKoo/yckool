83a3181bef8dedccaa6decbcb8dcfa23f71c7c3c
Indekxx <eugenekoo777@gmail.com>
2020-10-12 17:02:54 -0400

    added particles background

--- src/app/my-website/my-website.component.ts
+++ src/app/my-website/my-website.component.ts
@@ -1,25 +1,11 @@
-import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-website',
   templateUrl: './my-website.component.html',
   styleUrls: ['./my-website.component.scss']
 })
-export class MyWebsiteComponent implements AfterViewInit {
 
-  public randomNumber: number;
-  public photos: string[] = [
-    'img-sao-aincrad',
-    'img-sao-aincrad-logcabin'
-  ];
-  public image: string;
-
-  constructor(private cdr: ChangeDetectorRef) {}
-
-  ngAfterViewInit() {
-    this.randomNumber = Math.floor(Math.random() * 2);
-    this.image = this.photos[this.randomNumber];
-    this.cdr.detectChanges();
-  }
+export class MyWebsiteComponent {
 
 }
