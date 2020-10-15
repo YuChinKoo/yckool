3591d0f5b97e351e43233052b3b2db978fcd18a8
Eugene Koo <eugenekoo777@gmail.com>
2020-10-14 22:11:29 -0400

    Created console header tab component, and deleted unused components

--- src/app/my-website/my-website-routing.module.ts
+++ src/app/my-website/my-website-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { MyWebsiteRoutingConstants } from './my-website-routing-constants';
-import { ResumePageComponent } from './resume-page/resume-page.component';
 import { MyWebsiteComponent } from './my-website.component';
 
 const routes: Routes = [
@@ -13,14 +11,6 @@ const routes: Routes = [
     children: [{
         path: MyWebsiteRoutingConstants.homePage,
         component: HomePageComponent
-      },
-      {
-        path: MyWebsiteRoutingConstants.projectsPage,
-        component: ProjectsPageComponent
-      },
-      {
-        path: MyWebsiteRoutingConstants.resumePage,
-        component: ResumePageComponent
       }]
   }
 ];
--- src/app/my-website/my-website.module.ts
+++ src/app/my-website/my-website.module.ts
@@ -1,32 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
-import { ProjectsPageComponent } from './projects-page/projects-page.component';
-import { ResumePageComponent } from './resume-page/resume-page.component';
-import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 import { MyWebsiteRoutingModule } from './my-website-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MyWebsiteComponent } from './my-website.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ContactDialogComponent } from '../components/contact-dialog/contact-dialog.component';
-import { AboutSectionComponent } from '../components/about-section/about-section.component';
 import { FrontSectionComponent } from '../components/front-section/front-section.component';
 import { CommandPromptComponent } from './command-prompt/command-prompt.component';
+import { ConsoleHeaderComponent } from '../components/console-header/console-header.component';
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    ProjectsPageComponent,
-    ResumePageComponent,
-    HeaderComponent,
     FooterComponent,
     MyWebsiteComponent,
-    ContactDialogComponent,
-    AboutSectionComponent,
     FrontSectionComponent,
-    CommandPromptComponent
+    CommandPromptComponent,
+    ConsoleHeaderComponent
   ],
   imports: [
     CommonModule,
